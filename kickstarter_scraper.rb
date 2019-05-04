3ace66be27f2226a2326db9cb8a0e61d060762b5
Anthony Rurup <tcrurup@gmail.com>
2019-05-04 20:58:09 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,6 +3,7 @@ require 'pry'
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
+# image link 
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
