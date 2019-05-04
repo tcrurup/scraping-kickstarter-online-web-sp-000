66f73f5a11b1fdca2083399d5737813adf90de8c
Anthony Rurup <tcrurup@gmail.com>
2019-05-04 20:46:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokigiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
 
